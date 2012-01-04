1f120e440192c77706d26b5d0893a175e73fb085
Nathaniel Schweinberg <nathaniel@fightthecurrent.org>
2012-01-04 11:54:08 -0600

    Uncommented out the Month Year Generator section, Fixed positioning of Month Day, Year

--- Months.jsx
+++ Months.jsx
@@ -39,23 +39,23 @@ textWidth = 200;
 var doc = app.documents.add(width, height, resolution, docName, NewDocumentMode.RGB);
 
 //Create the Dates Month Year
-//for(i=0;i<12;i++){
-    //monthName = doc.artLayers.add();
-    //monthName.kind = LayerKind.TEXT;
-    //monthName.name = months[i];
-    //monthName.textItem.color = normalColor;
-    //monthName.textItem.size = 24;
-    //monthName.textItem.kind = TextType.PARAGRAPHTEXT;
-    //monthName.textItem.justification = Justification.LEFT;
-    //monthName.textItem.font = 'TradeGothicLTStd-BdCn20';
-    //monthName.textItem.capitalization = TextCase.ALLCAPS;
-    //monthName.textItem.height = textHeight;
-    //monthName.textItem.width = textWidth;
-    //monthName.textItem.contents = months[i]+' '+year;
-    //monthName.textItem.position = new Array(0, (5*i));
-//}
+for(i=0;i<12;i++){
+    monthName = doc.artLayers.add();
+    monthName.kind = LayerKind.TEXT;
+    monthName.name = months[i];
+    monthName.textItem.color = normalColor;
+    monthName.textItem.size = 24;
+    monthName.textItem.kind = TextType.PARAGRAPHTEXT;
+    monthName.textItem.justification = Justification.LEFT;
+    monthName.textItem.font = 'TradeGothicLTStd-BdCn20';
+    monthName.textItem.capitalization = TextCase.ALLCAPS;
+    monthName.textItem.height = textHeight;
+    monthName.textItem.width = textWidth;
+    monthName.textItem.contents = months[i]+' '+year;
+    monthName.textItem.position = new Array(0, (18*i));
+}
 var pos = 0;
-//Create the Dates Month Day Year
+//Create the Dates Month Day, Year
 for(curr=0;curr<12;curr++){
     for(i=0;i<2;i++){
         monthName = doc.artLayers.add();
@@ -68,7 +68,7 @@ for(curr=0;curr<12;curr++){
         monthName.textItem.capitalization = TextCase.ALLCAPS;
         monthName.textItem.height = textHeight;
         monthName.textItem.width = textWidth;
-        monthName.textItem.position = new Array(0, pos);
+        monthName.textItem.position = new Array(180, pos);
         pos+= 18; 
         x = i == 0 ? 2 : 4;
         date = getDateInterval(x, 3, curr, year);
